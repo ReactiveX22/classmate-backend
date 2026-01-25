f2525584c3beaf39b83db6105745ae22f9e2a55e
ReactiveX22 <akibreactive@gmail.com>
2026-01-25 22:28:03 +0600

    feat: add notice module scaffolding

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { NoticeService } from './notice.service';
+import { NoticeController } from './notice.controller';
 
-@Module({})
+@Module({
+  providers: [NoticeService],
+  controllers: [NoticeController]
+})
 export class NoticeModule {}
