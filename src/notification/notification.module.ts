579b7dd4a80c93212e3f53f92a809fefc531ebd5
ReactiveX22 <akibreactive@gmail.com>
2026-02-01 14:39:50 +0600

    feat: add notification gateway with authentication

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { MailModule } from 'src/mail/mail.module';
+import { NotificationGateway } from './notification.gateway';
 import { NotificationRepository } from './notification.repository';
 import { NotificationService } from './notification.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, MailModule],
-  providers: [NotificationService, NotificationRepository],
+  imports: [DatabaseModule, MailModule, AuthModule],
+  providers: [NotificationService, NotificationRepository, NotificationGateway],
 })
 export class NotificationModule {}
