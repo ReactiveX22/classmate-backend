c672d3fc6ddb04cbdef6a0daeade2ca21be4fcf7
ReactiveX22 <akibreactive@gmail.com>
2026-02-01 12:39:37 +0600

    feat: add delete and update notice API endpoint

--- src/notice/notice.repository.ts
+++ src/notice/notice.repository.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { and, eq } from 'drizzle-orm';
 import { type DB, InjectDb } from 'src/database/db.provider';
 import { type InsertNotice, notice } from 'src/database/schema';
 
@@ -8,6 +9,27 @@ export class NoticeRepository {
 
   async create(data: InsertNotice) {
     const [result] = await this.db.insert(notice).values(data).returning();
+    return result;
+  }
+
+  async update(
+    organizationId: string,
+    id: string,
+    data: Partial<InsertNotice>,
+  ) {
+    const [result] = await this.db
+      .update(notice)
+      .set(data)
+      .where(and(eq(notice.id, id), eq(notice.organizationId, organizationId)))
+      .returning();
+    return result;
+  }
+
+  async delete(organizationId: string, id: string) {
+    const [result] = await this.db
+      .delete(notice)
+      .where(and(eq(notice.id, id), eq(notice.organizationId, organizationId)))
+      .returning();
     return result;
   }
 }
