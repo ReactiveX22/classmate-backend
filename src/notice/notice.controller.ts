c288feb44f567a3c5fee3041502eb63d086ae385
ReactiveX22 <akibreactive@gmail.com>
2026-02-01 12:20:07 +0600

    feat: add create notice API endpoint

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Roles, Session } from '@thallesp/nestjs-better-auth';
+import { AppRole } from 'src/common/enums/role.enum';
+import { type AppUserSession } from 'src/common/types/session.types';
+import { CreateNoticeDto } from './dto/create-notice.dto';
+import { NoticeService } from './notice.service';
 
-@Controller('notice')
-export class NoticeController {}
+@Controller('notices')
+export class NoticeController {
+  constructor(private readonly noticeService: NoticeService) {}
+
+  @Roles([AppRole.Admin, AppRole.SuperAdmin])
+  @Post()
+  async createNotice(
+    @Body() dto: CreateNoticeDto,
+    @Session() session: AppUserSession,
+  ) {
+    return this.noticeService.create(dto, session.user);
+  }
+}
