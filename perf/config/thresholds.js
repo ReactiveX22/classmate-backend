60ef919d764e403e6882786df7863336340f967b
ReactiveX22 <akibreactive@gmail.com>
2026-09-25 22:42:42 +0600

    fix: update perf thresholds

--- perf/config/thresholds.js
+++ perf/config/thresholds.js
@@ -14,7 +14,7 @@ export const thresholds = {
     common: {
       http_req_duration: ['p(95)<300', 'p(99)<800'],
       'http_req_duration{endpoint:signup}': ['p(95)<8000'],
-      'http_req_duration{endpoint:signin}': ['p(95)<850'],
+      'http_req_duration{endpoint:signin}': ['p(95)<600'],
       'http_req_duration{endpoint:session}': ['p(95)<500'],
       'http_req_duration{endpoint:grade_stats}': ['p(95)<5000'],
       'http_req_duration{endpoint:classrooms_list}': ['p(95)<150'],
